e38f7c1f6b814f2eab9d027998d0c71a4e6ad59d
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-03-30 17:33:07 -0400

    upd
    "api" removed from endpoint,
    added "Not authorized" for bed token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,22 +9,32 @@ const { JWT_SECRET } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return next(HttpError(401, "Authoriztion header not found"));
+    return next(HttpError(401, "Not authorized"));
   }
 
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
     return next(HttpError(401));
   }
+  const verifyToken = jwt.verify(token, JWT_SECRET, function (err) {
+    if (err) {
+      return false;
+    }
+    return true;
+  });
+  if (!verifyToken) {
+    return next(HttpError(401, "Not authorized"));
+  }
 
   try {
     const { id } = jwt.verify(token, JWT_SECRET);
+
     const user = await findUser({ _id: id });
     if (!user) {
-      return next(HttpError(401, "User not found"));
+      return next(HttpError(401, "Not authorized"));
     }
     if (!user.token) {
-      return next(HttpError(401, "Token invalid"));
+      return next(HttpError(401, "Not authorized"));
     }
     req.user = user;
     next();
